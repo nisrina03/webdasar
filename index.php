274fa04272f06fa3860056243787d534dd803d3d
nisrina03 <nisrinacan@gmail.com>
2023-04-13 01:16:31 +0700

    penambahan index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+
+$nama = "Nisrina";
+$umur = 20;
+
+echo "Hello World, Welcome ".$nama." Umur ".$umur." tahun";
+echo "<br/>";
+echo "berarti $nama lahir di tahun ";
+echo 2023-$umur;
+
+?>
